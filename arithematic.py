012db0d6d17c7714fd941bb3fa86e7f48bb6c9f1
kirtikapei <43905950+kirtikapei@users.noreply.github.com>
2018-11-03 00:14:53 +0530

    Add files via upload

---
+++ arithematic.py
@@ -0,0 +1,11 @@
+x=int(input("entre the value of x:"))
+y=int(input("entre the value of y:"))
+opt=input("enter option:")
+if opt=='a':
+  a=x+y
+  print("the value of a",a)
+elif opt=='s':
+  s=x-y
+  print("the value of b",s)
+else:
+  print("x and y are invalid number")
